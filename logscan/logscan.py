f693a5e2a8e5b7455f5e3a6786a7ae5917369a40
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-10-12 20:26:46 -0300

    add benchmark

--- logscan/logscan.py
+++ logscan/logscan.py
@@ -11,6 +11,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import DBSCAN
 import operator
 import numpy as np
+import sys
+import os
+sys.path.append("../")
 
 nltk.download('punkt')
 regex_tokenizer = RegexpTokenizer(r'\w+')
@@ -141,7 +144,174 @@ def main():
   print(f"The package's version is: {__version__}")
 
   # Teste
-  android_dataset = pd.read_csv("logs\Andriod_2k.log_structured.csv")
+  android_dataset = pd.read_csv("logs/Andriod_2k.log_structured.csv")
   log_scan_android = LogScan(list(android_dataset['Content']), header=False)
   tagger_android, result_dataset = log_scan_android.pipeline()
+  result_dataset['EventId'] = android_dataset['EventId']
+  accuracy = parsing_accuracy(result_dataset)
   result_dataset.to_csv('resultados.csv')
+  benchmark()
+
+
+def parsing_accuracy(data):
+    log_per_template =  data['EventId'].value_counts().to_dict()
+    correct = 0
+    for cluster in np.unique(data['Cluster']):
+        data_cluster = data.loc[data['Cluster'] == cluster]
+        log_per_template_cluster =  data_cluster['EventId'].value_counts().to_dict()
+        for eventid in np.unique(data_cluster['EventId']):
+            if log_per_template[eventid] == log_per_template_cluster[eventid]:
+                correct = correct + log_per_template_cluster[eventid]
+    return correct/len(data)
+
+def benchmark():
+    input_dir = "logs/loghub_2k/"  # The input directory of log file
+    output_dir = "Logscan_result/"  # The output directory of parsing results
+
+    benchmark_settings = {
+        "HDFS": {
+            "log_file": "HDFS/HDFS_2k.log",
+            "log_format": "<Date> <Time> <Pid> <Level> <Component>: <Content>",
+            "regex": [r"blk_-?\d+", r"(\d+\.){3}\d+(:\d+)?"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "Hadoop": {
+            "log_file": "Hadoop/Hadoop_2k.log",
+            "log_format": "<Date> <Time> <Level> \[<Process>\] <Component>: <Content>",
+            "regex": [r"(\d+\.){3}\d+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "Spark": {
+            "log_file": "Spark/Spark_2k.log",
+            "log_format": "<Date> <Time> <Level> <Component>: <Content>",
+            "regex": [r"(\d+\.){3}\d+", r"\b[KGTM]?B\b", r"([\w-]+\.){2,}[\w-]+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "Zookeeper": {
+            "log_file": "Zookeeper/Zookeeper_2k.log",
+            "log_format": "<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>",
+            "regex": [r"(/|)(\d+\.){3}\d+(:\d+)?"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "OpenStack": {
+            "log_file": "OpenStack/OpenStack_2k.log",
+            "log_format": "<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>",
+            "regex": [r"((\d+\.){3}\d+,?)+", r"/.+?\s", r"\d+"],
+            "st": 0.5,
+            "depth": 5,
+        },
+        "BGL": {
+            "log_file": "BGL/BGL_2k.log",
+            "log_format": "<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>",
+            "regex": [r"core\.\d+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "HPC": {
+            "log_file": "HPC/HPC_2k.log",
+            "log_format": "<LogId> <Node> <Component> <State> <Time> <Flag> <Content>",
+            "regex": [r"=\d+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "Thunderbird": {
+            "log_file": "Thunderbird/Thunderbird_2k.log",
+            "log_format": "<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>",
+            "regex": [r"(\d+\.){3}\d+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "Windows": {
+            "log_file": "Windows/Windows_2k.log",
+            "log_format": "<Date> <Time>, <Level>                  <Component>    <Content>",
+            "regex": [r"0x.*?\s"],
+            "st": 0.7,
+            "depth": 5,
+        },
+        "Linux": {
+            "log_file": "Linux/Linux_2k.log",
+            "log_format": "<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>",
+            "regex": [r"(\d+\.){3}\d+", r"\d{2}:\d{2}:\d{2}"],
+            "st": 0.39,
+            "depth": 6,
+        },
+        "Mac": {
+            "log_file": "Mac/Mac_2k.log",
+            "log_format": "<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>",
+            "regex": [r"([\w-]+\.){2,}[\w-]+"],
+            "st": 0.7,
+            "depth": 6,
+        },
+        "Android": {
+            "log_file": "Android/Android_2k.log",
+            "log_format": "<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>",
+            "regex": [
+                r"(/[\w-]+)+",
+                r"([\w-]+\.){2,}[\w-]+",
+                r"\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b",
+            ],
+            "st": 0.2,
+            "depth": 6,
+        },
+        "HealthApp": {
+            "log_file": "HealthApp/HealthApp_2k.log",
+            "log_format": "<Time>\|<Component>\|<Pid>\|<Content>",
+            "regex": [],
+            "st": 0.2,
+            "depth": 4,
+        },
+        "Apache": {
+            "log_file": "Apache/Apache_2k.log",
+            "log_format": "\[<Time>\] \[<Level>\] <Content>",
+            "regex": [r"(\d+\.){3}\d+"],
+            "st": 0.5,
+            "depth": 4,
+        },
+        "OpenSSH": {
+            "log_file": "OpenSSH/OpenSSH_2k.log",
+            "log_format": "<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>",
+            "regex": [r"(\d+\.){3}\d+", r"([\w-]+\.){2,}[\w-]+"],
+            "st": 0.6,
+            "depth": 5,
+        },
+        "Proxifier": {
+            "log_file": "Proxifier/Proxifier_2k.log",
+            "log_format": "\[<Time>\] <Program> - <Content>",
+            "regex": [
+                r"<\d+\ssec",
+                r"([\w-]+\.)+[\w-]+(:\d+)?",
+                r"\d{2}:\d{2}(:\d{2})*",
+                r"[KGTM]B",
+            ],
+            "st": 0.6,
+            "depth": 3,
+        },
+    }
+
+    benchmark_result = []
+    for dataset, setting in benchmark_settings.items():
+        print("\n=== Avaliando: %s ===" % dataset)
+        indir = os.path.join(input_dir, os.path.dirname(setting["log_file"]))
+        log_file = os.path.basename(setting["log_file"])
+
+        test_dataset = pd.read_csv(os.path.join(indir, log_file + "_structured.csv"))
+        log_scan_android = LogScan(list(test_dataset['Content']), header=False)
+        tagger_android, result_dataset = log_scan_android.pipeline()
+        result_dataset['EventId'] = test_dataset['EventId']
+        result_dataset.to_csv(os.path.join(output_dir, log_file + "_structured.csv"))
+
+        accuracy = parsing_accuracy(result_dataset)
+        benchmark_result.append([dataset, accuracy])
+
+
+    print("\n=== Resultados ===")
+    df_result = pd.DataFrame(benchmark_result, columns=["Dataset", "Accuracy"])
+    df_result.set_index("Dataset", inplace=True)
+    print(df_result)
+    df_result.to_csv("Logscan_benchmark_result.csv", float_format="%.6f")
+
+
